62a67b235d3122ea21548d6a76c0689ee76ea0a0
Noé Plantier <noeplantier@MacBook-Air-de-Noe.local>
2025-01-08 19:36:33 +0100

    Video integration background / Main Title

--- src/components/premium/PremiumPlans.tsx
+++ src/components/premium/PremiumPlans.tsx
@@ -63,7 +63,7 @@ export function PremiumPlans() {
   
   return (
     
-    <div className="py-12 bg-gray-50">
+    <div className="py-12 bg-gray-50 mt-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
--- src/components/title/MainTitle.tsx
+++ src/components/title/MainTitle.tsx
@@ -2,62 +2,81 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal, Button, Form } from 'react-bootstrap';
 
-
 const MainTitle = () => {
+  const [showLoginModal, setShowLoginModal] = useState(false);
 
-const [showLoginModal, setShowLoginModal] = useState(false);
+  const handleOpenLoginModal = () => {
+    setShowLoginModal(true);
+  };
 
-const handleOpenLoginModal = () => {
-  setShowLoginModal(true);
-};
+  const handleCloseLoginModal = () => {
+    setShowLoginModal(false);
+  };
 
-const handleCloseLoginModal = () => {
-  setShowLoginModal(false);
-};
+  return (
+    <div className="relative min-h-screen">
+      {/* Vidéo de fond */}
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+      >
+        <source
+          src="/public/videos/204565-924698132_small.mp4" // Chemin vers votre vidéo locale
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
 
-
-return (
-
-<div className="flex justify-center items-center min-h-screen">
-        <div className="text-center my-2 p-6" id='main-container'>
-          <h1 className="text-purple-700 text-9xl font-bold" id='main-title'>WestApp</h1>
-          <h2 className="text-gray-600 text-4xl mt-4">Passez de l'URL à l'IRL</h2>
+      {/* Contenu principal */}
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="text-center my-2 p-6" id="main-container">
+          <h1 className="text-white text-9xl font-bold" id="main-title">
+            WestApp
+          </h1>
+          <h2 className="text-gray-200 text-4xl mt-4">
+            Passez de l'URL à l'IRL
+          </h2>
           <div className="mt-8">
-            <a href="/explanations" className="btn btn-primary mr-4">Get Started</a>
-            <Button variant="secondary" onClick={handleOpenLoginModal}>Se Connecter</Button>
-    
-
-    <Modal show={showLoginModal} onHide={handleCloseLoginModal}>
-      <Modal.Header closeButton>
-        <Modal.Title>Connexion</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleCloseLoginModal}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
-    </div>
+            <Button className="btn btn-primary mr-4">Get Started</Button>
+            <Button variant="secondary" onClick={handleOpenLoginModal}>
+              Se Connecter
+            </Button>
+          </div>
         </div>
       </div>
-);
-}
+
+      {/* Modale Connexion */}
+      <Modal show={showLoginModal} onHide={handleCloseLoginModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Connexion</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseLoginModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
 
 export default MainTitle;
