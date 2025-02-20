c71cc82c730ccec7fecaa7cdc1711e64edf5537d
Noé Plantier <plantiernoe50@gmail.com>
2025-02-20 23:51:19 +0100

    Clerk Key integration

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import { ClerkProvider } from '@clerk/clerk-react'
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error("Missing Publishable Key")
+// }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
